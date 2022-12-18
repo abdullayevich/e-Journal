1fc14e59a08d59fcab74a5f1c7d4f4776209b43a
Normadjon <gofforovnormadjon@gmail.com>
2022-12-18 17:47:39 +0500

    detached

--- src/e-Journal/EJournal.Service/Dtos/Students/StudentCreateDto.cs
+++ src/e-Journal/EJournal.Service/Dtos/Students/StudentCreateDto.cs
@@ -11,12 +11,12 @@ namespace EJournal.Service.Dtos.Students
         public string FullName { get; set; } = string.Empty;
         
         public IFormFile? ImagePath { get; set; }
-        [Required,EmailAddress]
+        [Required,EmailAddress] 
         public string Email { get; set; } = string.Empty;
         [Required, StrongPassword(ErrorMessage = "Password must contain both capital and small characters, number and length should be more then 8")]
         public string Password { get; set; } = string.Empty;
 
-        public int GroupId { get; set; }
+        public int GroupId { get; set; } 
 
         public static implicit operator Student(StudentCreateDto dto)
         {
